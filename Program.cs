78c9f756b8381e00bc706acbbf4f5806722d60fc
Palexander10 <pa.ramonm@uea.edu.ec>
2026-06-16 18:31:03 -0500

    feat: commit inicial del sistema de agenda clinica

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿C#
+using System;
+
+namespace AgendaClinica
+{
+    // Estructura 1: Registro (Struct) para agrupar datos del paciente
+    public struct Paciente
+    {
+        public string Cedula;
+        public string Nombre;
+        public string Especialidad;
+    }
+
+    // Clase 1: Representa la entidad Turno
+    public class Turno
+    {
+        public bool Ocupado { get; set; }
+        public Paciente DatosPaciente { get; set; }
+
+        public Turno()
+        {
+            Ocupado = false; // Por defecto el turno está libre
+        }
+    }
